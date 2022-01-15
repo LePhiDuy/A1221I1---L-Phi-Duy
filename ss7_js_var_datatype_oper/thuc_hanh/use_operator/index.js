1fe242862d0f86ed5ec9867b47460a1a8cdcec66
LePhiDuy <duyphi2011@gmail.com>
2022-01-15 23:04:24 +0700

    ss7

---
+++ ss7_js_var_datatype_oper/thuc_hanh/use_operator/index.js
@@ -0,0 +1,7 @@
+var inputWidth, inputHeight;
+inputWidth = prompt('Nhập vào chiều dài');
+inputHeight = prompt('Nhập vào chiều rộng');
+var width = parseInt(inputWidth);
+var length = parseInt(inputHeight);
+var S = width * length;
+document.write('S = ' + S);
